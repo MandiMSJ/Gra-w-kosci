1a367c53cc2d4a9f3a4f6b1ee1db3fcba19a03f2
egzamin <egzamin@DESKTOP-B66160U>
2023-03-09 15:29:27 +0100

    Dodaj pliki projektów.

---
+++ Gra w kości/Dice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gra_w_kości
+{
+    public class Dice
+    {
+        public int Value { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
